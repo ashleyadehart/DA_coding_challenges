8a63871eea0dda84c929f2ad865ad53dcf268fbb
Ashley A. Dehart <152433948+ashleyadehart@users.noreply.github.com>
2026-03-27 17:37:24 -0400

    Added test cases to code to ensure that it works correctly

--- maximum_number.py
+++ maximum_number.py
@@ -1,17 +1,20 @@
-# Define a function takes a list of numbers and returns the maximum number in the list.
+#TODO: Define a function that takes a list of numbers and returns the maximum number in the list.
 def find_max(numbers):
     
-# Check if the list is empty
+#TODO: Check if the list is empty
     if len(numbers) == 0:
         raise ValueError("The list cannot be empty.")
     
-# Initialize the maximum with the first element
+#TODO: Initialize the maximum with the first element
     current_max = numbers[0]
     
-# Loop through each number in the list
+#TODO: Loop through each number in the list
     for num in numbers:
         if num > current_max:
             current_max = num
     return current_max
 
+#TODO: Test the function with different lists of numbers to ensure it works correctly.
 print(find_max([4, 12, 2, 20, 5]))
+print(find_max([-1, -5, -3, -4]))
+print(find_max([7, 7, 7, 7]))
