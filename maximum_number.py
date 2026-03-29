ed8d7bfd2e224b9a2b762ffbcea9132dccd30c2a
Ashley A. Dehart <152433948+ashleyadehart@users.noreply.github.com>
2026-03-29 10:55:37 -0400

    Edited Python structure

--- maximum_number.py
+++ maximum_number.py
@@ -1,5 +1,7 @@
+import math
+
 #TODO: Define a function that takes a list of numbers and returns the maximum number in the list.
-def find_max(numbers):
+def find_max(numbers: list) -> int:
     
 #TODO: Check if the list is empty
     if len(numbers) == 0:
@@ -15,6 +17,14 @@ def find_max(numbers):
     return current_max
 
 #TODO: Test the function with different lists of numbers to ensure it works correctly.
-print(find_max([4, 12, 2, 20, 5]))
-print(find_max([-1, -5, -3, -4]))
-print(find_max([7, 7, 7, 7]))
+def main() -> None:
+    try:
+        print(find_max([4, 12, 2, 20, 5]))
+        print(find_max([-1, -5, -3, -4]))
+        print(find_max([7, 7, 7, 7]))
+
+    except ValueError as e:
+        print(f"Error: {e}")
+
+if __name__ == "__main__":
+    main()
